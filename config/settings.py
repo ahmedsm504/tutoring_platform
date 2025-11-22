cbd59e74c522388dcb143d0b2e6e4a1e20c74cb7
ahmedsm504 <ahmedsm504@gmail.com>
2025-11-22 14:18:34 +0200

    Add Procfile and gunicorn

--- config/settings.py
+++ config/settings.py
@@ -221,17 +221,6 @@ cloudinary.config(
 
 SITE_ID = 1
 
-# Security Settings (للإنتاج فقط)
-if not DEBUG:
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    X_FRAME_OPTIONS = 'DENY'
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
 
 # Cache للـ Sitemap (اختياري)
 CACHES = {
