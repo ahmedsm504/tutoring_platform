594446fa9a806446eac6ab57da80d77d508bb4bd
ahmedsm504 <ahmedsm504@gmail.com>
2025-11-24 11:40:46 +0200

    Fix HTTPS/SSL configuration

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ ALLOWED_HOSTS = [
 CSRF_TRUSTED_ORIGINS = [
     'https://alagme.com',
     'https://www.alagme.com',
+    'www.alagme.com',
     'https://tutoringplatform-production-b8c8.up.railway.app',
 ]
 
