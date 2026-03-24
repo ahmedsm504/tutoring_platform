16309f6d02f0041d0383a12ebed82cabb1ecd262
ahmedsm504 <ahmedsm504@gmail.com>
2026-03-25 01:34:38 +0200

    Fix robots.txt to accept HEAD requests

--- accounts/notifications.py
+++ accounts/notifications.py
@@ -1,9 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 from django.conf import settings
+import os
+import json
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(settings.FIREBASE_CREDENTIALS)
+    firebase_json = os.environ.get("FIREBASE_CREDENTIALS_JSON")
+    cred = credentials.Certificate(json.loads(firebase_json))
     firebase_admin.initialize_app(cred)
 
 
