edbb909631b161d8be6241b98175169cc8a261ed
ahmedsm504 <ahmedsm504@gmail.com>
2026-03-25 13:19:09 +0200

    Fix robots.txt to accept HEAD requests

--- qna/signals.py
+++ qna/signals.py
@@ -1,24 +1,34 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from .models import PublicQuestion
 from accounts.notifications import send_public_notification
 from django.db import transaction
 
+
 @receiver(pre_save, sender=PublicQuestion)
-def notify_question_approved(sender, instance, **kwargs):
-    if not instance.pk:
+def cache_old_status(sender, instance, **kwargs):
+    """احفظ الـ status القديم قبل الحفظ"""
+    if instance.pk:
+        try:
+            old = PublicQuestion.objects.get(pk=instance.pk)
+            instance._old_status = old.status
+        except PublicQuestion.DoesNotExist:
+            instance._old_status = None
+    else:
+        instance._old_status = None
+
+
+@receiver(post_save, sender=PublicQuestion)
+def notify_question_approved(sender, instance, created, **kwargs):
+    """ابعت إشعار لما السؤال يتعتمد"""
+    if created:
         return
 
-    try:
-        old = PublicQuestion.objects.get(pk=instance.pk)
+    old_status = getattr(instance, '_old_status', None)
 
-        if old.status != 'approved' and instance.status == 'approved':
-            # 🔥 نخلي الإرسال بعد الحفظ
-            transaction.on_commit(lambda: send_public_notification(
-                title=f'❓ سؤال جديد: {instance.title[:50]}',
-                message=instance.question_text[:100],
-                url=f'https://alagme.com/questions/question/{instance.slug}/'
-            ))
-
-    except PublicQuestion.DoesNotExist:
-        pass
+    if old_status and old_status != 'approved' and instance.status == 'approved':
+        transaction.on_commit(lambda: send_public_notification(
+            title=f'❓ سؤال جديد: {instance.title[:50]}',
+            message=instance.question_text[:100],
+            url=f'https://alagme.com/questions/question/{instance.slug}/'
+        ))
