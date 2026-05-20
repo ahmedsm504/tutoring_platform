8709c4ce46a97ae220b21e58cfe7c5a3713cfb18
ahmedsm504 <ahmedsm504@gmail.com>
2026-05-21 00:20:28 +0300

    update project

--- qna/views.py
+++ qna/views.py
@@ -23,8 +23,8 @@ class QuestionListView(ListView):
     model = PublicQuestion
     template_name = 'qna/question_list.html'
     context_object_name = 'questions'
-    paginate_by = 15
-
+    paginate_by = 10
+    
     def get_queryset(self):
         queryset = PublicQuestion.objects.filter(status='approved')
 
